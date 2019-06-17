a634b86769a9ed2a583e5ee94bad29ebf17e1fed
张金伟 <guider@yeah.net>
2019-06-17 16:39:22 +0800

    format

--- src/extension.ts
+++ src/extension.ts
@@ -15,15 +15,18 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.createInexitenceFile', () => {
         console.log('vscode.window');
         console.log(vscode.window.activeTextEditor);
+        console.log(vscode.window);
         console.log('vscode.window');
+        let editor = vscode.window.activeTextEditor;
 
+        console.log(editor.document.lineAt(0).text);
+        console.log(editor.selection.active.line);
+        let line = editor.document.lineAt(editor.selection.active.line).text;
         // The code you place here will be executed every time your command is executed
-
-        let line = vscode.window.activeTextEditor['_documentData']['_lines'][0];
+        // let line = vscode.window.activeTextEditor._documentData.lines[0];
         console.log(line);
-        let reg = /'(.*)'$/;
+        let reg = /'(.*)'|"(.*)"/;
         let path = line.match(reg);
-
         console.log(path);
         let app = new AppModel();
         app.createFileOrFolder('file', path[1]);
